741172e5bdbedaf20a9bdf35440c82222adb50b2
Huy Trịnh <trinhnhathuy3@gmail.com>
2024-10-16 15:54:20 +0700

    Change sth

--- src/pages/mentorPage/Mentor.jsx
+++ src/pages/mentorPage/Mentor.jsx
@@ -6,70 +6,79 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { fetchMentorData } from "../../redux/features/mentor/mentorSlice";
 function Mentor() {
-  const dispatch = useDispatch();
-  const { mentors, loading, error } = useSelector((state) => state.mentor);
+    const dispatch = useDispatch();
+    const { mentors, loading, error } = useSelector((state) => state.mentor);
 
-  useEffect(() => {
-    dispatch(fetchMentorData());
-  }, [dispatch]);
+    useEffect(() => {
+        dispatch(fetchMentorData());
+    }, [dispatch]);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+    if (loading) {
+        return <div>Loading...</div>;
+    }
 
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-  return (
-    <div className="mentor__page">
-      <div className="container">
-        <div className="mentor__page-inner">
-          <div className="mentor__page-title">Our mentor</div>
-          <div className="mentor__page-group">
-            {mentors.length > 0 &&
-              mentors.map((mentor, index) => (
-                <div className="mentor__page-item">
-                  <Link to="/mentorDetail">
-                    {" "}
-                    <img src={mentor.avatar} alt="" className="mentor__img" />
-                  </Link>
-                  <button className="mentor__page-liked">
-                    <ButtonLike />
-                  </button>
-                  <div className="mentor__info">
-                    {/* {mentor.enumList.map((major, index) => (
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+    return (
+        <div className="mentor__page">
+            <div className="container">
+                <div className="mentor__page-inner">
+                    <div className="mentor__page-title">Our mentor</div>
+                    <div className="mentor__page-group">
+                        {mentors.length > 0 &&
+                            mentors.map((mentor, index) => (
+                                <div className="mentor__page-item" key={index}>
+                                    <Link to="/mentorDetail">
+                                        {" "}
+                                        <img
+                                            src={mentor.avatar}
+                                            alt=""
+                                            className="mentor__img"
+                                        />
+                                    </Link>
+                                    <button className="mentor__page-liked">
+                                        <ButtonLike />
+                                    </button>
+                                    <div className="mentor__info">
+                                        {/* {mentor.enumList.map((major, index) => (
                       <div className="mentor__major">{major}</div>
                     ))} */}
-                    <div className="mentor__major">
-                      {mentor.enumList.join(" | ")}
+                                        <div className="mentor__major">
+                                            {mentor.enumList.join(" | ")}
+                                        </div>
+                                        <div className="mentor__name">
+                                            <Link
+                                                to="/mentorDetail"
+                                                className="name"
+                                            >
+                                                {mentor.accountName}
+                                            </Link>
+                                            <img
+                                                src={tick}
+                                                alt=""
+                                                className="tick"
+                                                style={{ marginTop: "5px" }}
+                                            />
+                                        </div>
+                                        <div className="mentor__email">
+                                            <img
+                                                src={mentor__icon}
+                                                alt=""
+                                                className="mentor__email-icon"
+                                            />
+                                            <p className="email">
+                                                {mentor.accountEmail}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
                     </div>
-                    <div className="mentor__name">
-                      <Link to="/mentorDetail" className="name">
-                        {mentor.accountName}
-                      </Link>
-                      <img
-                        src={tick}
-                        alt=""
-                        className="tick"
-                        style={{ marginTop: "5px" }}
-                      />
-                    </div>
-                    <div className="mentor__email">
-                      <img
-                        src={mentor__icon}
-                        alt=""
-                        className="mentor__email-icon"
-                      />
-                      <p className="email">{mentor.accountEmail}</p>
-                    </div>
-                  </div>
                 </div>
-              ))}
-          </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Mentor;
