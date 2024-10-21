cfb7df5dbb0ef53b87e84fdd68713f3bdaeeb8eb
Huy Trịnh <trinhnhathuy3@gmail.com>
2024-10-21 23:10:25 +0700

    Drag and drop in 1 columns

--- src/pages/MemberManagement/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
+++ src/pages/MemberManagement/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
@@ -18,6 +18,7 @@ function CardItem({ card }) {
         transform: CSS.Translate.toString(transform),
         transition,
         opacity: isDragging ? 0.5 : undefined,
+        border: isDragging ? "1px solid #2ecc71" : undefined,
     };
     return (
         <Card
