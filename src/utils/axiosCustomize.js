7d7a4d0381be88a3bf4240e58c5b55a54aeb151d
Huy Trịnh <trinhnhathuy3@gmail.com>
2024-11-08 21:03:54 +0700

    aa

--- src/utils/axiosCustomize.js
+++ src/utils/axiosCustomize.js
@@ -4,6 +4,9 @@ export const BASE_URL = import.meta.env.VITE_REACT_APP_API_URL;
 
 const instance = axios.create({
     baseURL: BASE_URL,
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
 // Add a request interceptor
